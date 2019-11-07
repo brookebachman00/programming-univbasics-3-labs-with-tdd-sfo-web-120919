ded771dbde6b9f0f88729ba4e42353b096d97920
Brooke Bachman <bbachman1994@gmail.com>
2019-11-07 22:49:20 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -4,4 +4,6 @@ second_number = 29
 
 sum = first_number + second_number
 
-difference = first_number * second_number
+difference = first_number - second_number
+
+product = first_number * second_number
